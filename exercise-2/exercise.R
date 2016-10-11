bfd84c9dadf7539efe9450f811575bcdc73dbb02
Jake George <Jake George>
2016-10-11 14:21:17 -0700

    Initial commit, completed exercise 2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,22 +1,24 @@
 # Exercise 2: Creating data frames
 
 # Create a vector of the number of points the Seahawks have scored in each game this season (google "Seahawks")
-
+points.scored <- c(12, 3, 37, 27)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored against them in each game this season
-
+points.against <- c(10, 9, 18, 17)
 
 # Combine your two vectors into a dataframe
-
+seahawks <- data.frame(points.scored, points.against)
 
 # Create a new column "diff" that is the difference in points
-
+seahawks$diff <- seahawks$points.scored - seahawks$points.against
 
 # Create a new column "won" which is TRUE if the Seahawks wom
-
+seahawks$won <- seahawks$points.scored > seahawks$points.against
 
 # Create a vector of the opponents
-
+opponents <- c('Dolphins', 'Rams', '49ers', 'Jets')
 
 # Assign your dataframe rownames of their opponents
+rownames(seahawks) <- opponents
+
 
